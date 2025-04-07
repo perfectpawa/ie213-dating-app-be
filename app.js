3b6725a63247765bbb71e2862ef0a131cd6daa36
perfectpawa <baotlg1005@gmail.com>
2025-04-08 01:42:44 +0700

    - setup server - ser error handle middleware

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongooseSanitize = require('express-mongo-sanitize');
 
-var app = express();
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 
-app.use(logger('dev'));
-app.use(express.json());
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use("/", express.static("uploads"))
+app.use(helmet());
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials: true
+    }
+));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(logger('dev'));
+}
+
+app.use(express.json({ limit: '10kb' }));
+
+app.use(mongooseSanitize());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.all('*', function(req, res, next) {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+})
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ server.js
@@ -0,0 +1,34 @@
+﻿const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+process.on('uncaughtException', function(err) {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    process.exit(1);
+})
+
+const app = require('./app');
+
+const mongoose = require('mongoose');
+mongoose
+    .connect(process.env.DB_URI)
+    .then(() => {
+        console.log("DB connection successful");
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', function(err) {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    server.close(() => {
+        process.exit(1);
+    });
+})
